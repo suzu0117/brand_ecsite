4fad71bcdd7e5c0151f08be53e23418ede87083d
suzu0117 <142565665+suzu0117@users.noreply.github.com>
2025-09-14 04:16:21 +0900

    Update connect_db.py

--- connect_db.py
+++ connect_db.py
@@ -1,12 +1,12 @@
-import pyscopg2
+import psycopg2
 import os
 
 def connect_db():
-    get_connection = pyscopg2.connect(
+    get_connection = psycopg2.connect(
         host = os.environ.get("DB_HOST"),
-        name = os.environ.get("DB_NAME"),
+        dbname = os.environ.get("DB_NAME"),
         user = os.environ.get("DB_USER"),
-        password = os.environ.get("DB_POSSWORD"),
+        password = os.environ.get("DB_PASSWORD"),
         port = os.environ.get("DB_PORT")
     )
     return get_connection
