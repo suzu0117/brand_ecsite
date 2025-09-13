1c36a9ccaef4f88eeac28c40d5873f631a877674
suzu0117 <142565665+suzu0117@users.noreply.github.com>
2025-09-14 04:12:51 +0900

    Update connect_db.py

--- connect_db.py
+++ connect_db.py
@@ -3,10 +3,10 @@ import os
 
 def connect_db():
     get_connection = pyscopg2.connect(
-        host = os.environ.get("DB_HOST")
-        name = os.environ.get("DB_NAME")
-        user = os.environ.get("DB_USER")
-        password = os.environ.get("DB_POSSWORD")
+        host = os.environ.get("DB_HOST"),
+        name = os.environ.get("DB_NAME"),
+        user = os.environ.get("DB_USER"),
+        password = os.environ.get("DB_POSSWORD"),
         port = os.environ.get("DB_PORT")
     )
     return get_connection
